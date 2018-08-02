bf063d5edb4640f2b872268bdfd57fcfa3814c1b
CecileRobertMichon <cerobert@microsoft.com>
2018-08-01 17:13:51 -0700

    remove makefile extra stuff

--- packer.mk
+++ packer.mk
@@ -1,12 +1,3 @@
-SHELL += -eu
-
-BLUE	:= \033[0;34m
-GREEN	:= \033[0;32m
-RED   := \033[0;31m
-NC    := \033[0m
-
-# ∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨∨
-
 build-packer:
 	@packer build -var-file=packer/settings.json packer/vhd-image.json
 
