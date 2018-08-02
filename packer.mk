656e275e83c5c7db04217bd097152504e635e192
CecileRobertMichon <cerobert@microsoft.com>
2018-08-02 14:28:04 -0700

    move az login back to makefile

--- packer.mk
+++ packer.mk
@@ -4,5 +4,11 @@ build-packer:
 init-packer:
 	@./packer/scripts/init-variables
 
+az-login:
+	az login --service-principal -u ${CLIENT_ID} -p ${CLIENT_SECRET} --tenant ${TENANT_ID}
+
 run-packer:
-	@packer version && make init-packer && make build-packer
+	@packer version && make az-login && make init-packer && make build-packer
+
+az-copy:
+	@make az-login && azcopy --help
